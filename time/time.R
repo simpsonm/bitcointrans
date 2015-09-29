ee7be1741a56e02f981af9f967ee3bd1c004cad8
Matt Simpson <themattsimpson@gmail.com>
2015-09-28 23:26:18 -0500

    Changed measurement error distribution to lognormal and... numerical problems evaporate. Probably a better choice anyway.

--- time/time.R
+++ time/time.R
@@ -20,7 +20,7 @@ timedata$elapsed_time <- timedata$time - c(NA,timedata$time[-length(timedata$tim
 timedata$min <- timedata$time/60 ## convert to minutes
 
 
-nobs <- 100 ## does not work very well for nobs > 100
+nobs <- 500 
 timedatashort <- timedata[(1000 - nobs + 1):1011,]
 lbs <- rep(0,nobs)
 for(i in 1:nobs){
@@ -33,14 +33,11 @@ which(lbs > times) ## check that each lower bound is below what it's bounding
 times <- times - timedatashort$min[11]
 lbs <- lbs - timedatashort$min[11]
 
-timelist <- list(nobs = nobs, nprev = 1, t = times, lb = lbs, lambda = 1/10)
+timelist <- list(nobs = nobs, t = times, lb = lbs, lambda = 1/10)
 ## lambda = 1 / mean block discovery time = 1/10 minutes
 
-inits <- list(list(x = rep(10, nobs), sigma = 1));
-
-fit <- stan(file = 'time.stan', data = timelist, iter=100, chains=4)
-
-system.time(fit2 <- stan(fit = fit, data = timelist, iter=10000, chains=1, init=inits))
+fit <- stan(file = 'time.stan', data = timelist, iter=1, chains=1)
+system.time(fit2 <- stan(fit = fit, data = timelist, iter=1000, chains=2))
 
 
 fit2out <- extract(fit2)
@@ -50,5 +47,8 @@ par(mfrow=c(4,4))
 traceplot(mcmc(fit2out$sigma))
 traceplot(mcmc(fit2out$tau[,1:15]))
 
-summary(mcmc(fit2out$tau[,40:50]))
-#####  for large sample sizes, sampler runs into trouble. Need to investigate
+summary(mcmc(fit2out$tau))
+
+#### lognormal seems to work fine, need to fit bigger model, also investigate other error
+#### distributions, e.g. gamma
+
--- time/time.stan
+++ time/time.stan
@@ -1,25 +1,29 @@
 data {
   int<lower=0> nobs;
-  int<lower=0> nprev;
+  //  int<lower=0> nprev;
   vector[nobs] t;
   vector[nobs] lb;
   real<lower=0> lambda;  // 1 / mean arrival time
 }
 parameters {
-  vector<lower=0>[nobs] x;
-  real<lower=0> sigma;
+  vector<lower=0.01>[nobs] x;
+  real<lower=0.01> sigma;
 }
 transformed parameters{
   //  positive_ordered[nobs] tau;
   vector[nobs] tau;
+  //  vector[nobs] alpha;
+  //  vector[nobs] beta;
   tau <- cumulative_sum(x);
+  //  beta <- tau / sigma^2;
+  //  alpha <- beta .* tau;
 }
 model {
-  t[1] ~ normal(tau[1], sigma) T[lb[1],]; 
-  x[1] ~ gamma(nprev, lambda);  // uses shape/rate parameterization
-  for(i in 2:nobs){
-    t[i] ~ normal(tau[i], sigma) T[lb[i],]; 
+  //  t[1] ~ normal(tau[1], sigma) T[lb[1],]; 
+  //  x[1] ~ gamma(nprev, lambda);  // uses shape/rate parameterization
+  for(i in 1:nobs){
+    t[i] ~ lognormal(log(tau[i]) - sigma^2/2, sigma) T[fmax(lb[i],0),]; 
     x[i] ~ exponential(lambda);
   }
-  sigma ~ cauchy(0, 2.5) T[0,];
+  sigma ~ cauchy(0, 2.5);
 }
